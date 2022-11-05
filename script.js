24177b62074c4526e37acf6a7d8decba7fc8371f
Subramanian Narayanan <101008478+subbuwu@users.noreply.github.com>
2022-11-05 18:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/**
+ * JS Code to get data from form and append to a table
+ * Clears the form once the data is appended
+ */
+
+
+// Get the required elements form the DOM
+const signupForm = document.querySelector('.main__form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const table = document.getElementById('table');
+
+// Initialize list count
+let count = 1;
+
+// Function to add a new entry to the table
+const handleAddNewUser = (e) => {
+    // Disable form default behavior to reload the page 
+    e.preventDefault();
+    // Get the email value from the input
+    const email = emailInput.value;
+    // Create a template to add the new entry
+    const newEntry = `
+        <tr>
+            <td>${count}.</td>
+            <td>${email}</td>
+        </tr>
+    `
+    // Append new entry to form as html
+    table.innerHTML += newEntry;
+
+    // Increment count and reset from
+    count++;
+    signupForm.reset();
+}
+
+// Executes the `handleAddNewUser` function every time the form is submitted
+signupForm.addEventListener('submit', handleAddNewUser);
